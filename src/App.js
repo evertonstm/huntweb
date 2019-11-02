929aeecf540c7f8a595bce721fcbb9b17dda51fd
Everton Reis <evertonreis@Mac-mini-de-Everton.local>
2019-11-02 18:32:32 -0400

    class App, Header

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1>Hello Rocketseat</h1>
+        <h1>Hello World</h1>
       </div>
     );
   }
-  
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+
+);
+
+class Header extends Component{
+  render(){
+    return <h1></h1>
+  }
+}
